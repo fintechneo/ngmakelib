8a0143fd73d5d846c2dacf061cef041bd1ed4ebe
Peter Johan Salomonsen <pjsalomonsen@gmail.com>
2018-03-01 12:53:24 +0100

    Also include the version number from cli command parameter

--- bin/ngmakelib.js
+++ bin/ngmakelib.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var NGMakeLib = require('./ngmakelib.api').NGMakeLib;
 
-new NGMakeLib(process.argv[2], process.argv[3], process.argv[4] ? '0.1.0' : process.argv[4])
+new NGMakeLib(process.argv[2], process.argv[3], process.argv[4] ? process.argv[4] : '0.1.0')
     .build()
     .then(function() { console.log("All done"); });
