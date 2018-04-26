b836045038b142874cc255123985edc113ac9cca
Peter Johan Salomonsen <pjsalomonsen@gmail.com>
2018-04-26 14:48:35 +0200

    Copy package.json from project dir

--- src/configs/packagejson.config.ts
+++ src/configs/packagejson.config.ts
@@ -1,20 +1,21 @@
 import { readFileSync } from 'fs';
 
 export class PackageJSONConfig {
-    public getConfig(moduleId: string, version?: string) : any {        
-        if(!version) {
-            try {                
-                version = JSON.parse(readFileSync('package.json').toString()).version;
-            } catch (e) {                
-                version = '0.1.0';
-                console.log(`Defaulting to version {{version}} since no version were provided, and couldn't find version in package.json of your project`);
-            }
+    public getConfig(moduleId: string, version?: string) : any {   
+        const packageJSON = JSON.parse(readFileSync('package.json').toString());     
+        if(!packageJSON.version && !version) {
+            packageJSON.version = '0.1.0';
+            console.log(`Defaulting to version {{version}} since no version were provided, and couldn't find version in package.json of your project`);
         }
-        return {
+        if(version) {
+            packageJSON.version = version;
+        }
+        Object.assign(packageJSON, {
             "name": moduleId,
-            "version": version,
             "main": moduleId+".js",
             "types": moduleId+".d.ts"
-        };
+        });
+        delete packageJSON.scripts;
+        return packageJSON;
     }
 }
